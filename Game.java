9eef0c410eea9ce5066ff028668b2ab43f8fa138
Daniel Binoy <dbinoy15@gmail.com>
2018-03-31 17:45:20 -0700

    made level 4 harder

--- Game.java
+++ Game.java
@@ -21,7 +21,7 @@ import java.io.IOException;
 public class Game extends JPanel implements KeyListener{
   int playerSize = 20;
   int playerState = 0;
-  int level = 0;
+  int level = 3;
   int where = 0;
   static int squareSize = 64;
   int moves = 0;
