80b4ec828892c05c5da942b08c75c289030a7446
Daniel Binoy <dbinoy15@gmail.com>
2018-04-01 21:31:07 -0700

    updater jar

--- src/AudioPlayerExample1.java
+++ src/AudioPlayerExample1.java
@@ -1,6 +1,6 @@
 import java.io.File;
 import java.io.IOException;
- 
+
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
@@ -10,7 +10,7 @@ import javax.sound.sampled.LineEvent;
 import javax.sound.sampled.LineListener;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
- 
+
 /**
  * This is an example program that demonstrates how to play back an audio file
  * using the Clip in Java Sound API.
@@ -31,41 +31,41 @@ public class AudioPlayerExample1 implements LineListener {
      * @param audioFilePath Path of the audio file.
      */
     public AudioPlayerExample1(Game g){
-     parent = g; 
+     parent = g;
     }
     void play(String audioFilePath) {
         File audioFile = new File(audioFilePath);
- 
+
         try {
             AudioInputStream audioStream = AudioSystem.getAudioInputStream(audioFile);
- 
+
             AudioFormat format = audioStream.getFormat();
- 
+
             DataLine.Info info = new DataLine.Info(Clip.class, format);
- 
+
             Clip audioClip = (Clip) AudioSystem.getLine(info);
             //BigClip audioClip = new BigClip(audioswoop);
-            
+
             audioClip.addLineListener(this);
 
             audioClip.open(audioStream);
-             
+
             audioClip.loop(-1);
-             
+
             while (!playCompleted) {
                 // wait for the playback completes
                 try {
                   parent.where = 0;
                   parent.tick();
                   Thread.sleep(bpm);
-                  
+
                 } catch (InterruptedException ex) {
                   //System.out.println("kill the jews");
                 }
             }
-             
+
             audioClip.close();
-             
+
         } catch (UnsupportedAudioFileException ex) {
           System.out.println("file type");
         } catch (LineUnavailableException ex) {
@@ -73,24 +73,20 @@ public class AudioPlayerExample1 implements LineListener {
         } catch (IOException ex) {
           System.out.println("io");
         }
-         
-    }
 
+    }
     @Override
     public void update(LineEvent event) {
         LineEvent.Type type = event.getType();
-         
+
         if (type == LineEvent.Type.START) {
             System.out.println("Playback started.");
-             
+
         } else if (type == LineEvent.Type.STOP) {
             //playCompleted = true;
             System.out.println("Playback completed.");
-            
-        }
- 
-    }
- 
 
- 
+        }
+
+    }
 }
