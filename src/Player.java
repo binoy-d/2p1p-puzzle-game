5a481f2e8e98273dd6a8c55cbd497967557f37ad
Kyle Pu <31393170+Kyle-P@users.noreply.github.com>
2018-04-01 20:23:54 -0700

    Reformat code

--- src/Player.java
+++ src/Player.java
@@ -19,67 +19,74 @@ import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
 import java.io.IOException;
 
-public class Player extends Point{
-    Game parentGame;
-  String[][] currentMap;
-  public Player(int x, int y,Game g){
-   super(x,y);
-   parentGame = g;
-   currentMap = parentGame.getCurrentMap();
-  }
-  public void checkEnemyTouch(){
-    for(Enemy e: parentGame.getEnemies()){
-      if(x == e.x && y == e.y){
-       parentGame.initialize(); 
-      }
-    }
-  }
-  public static boolean isInteger(String s) {
-    try { 
-      Integer.parseInt(s); 
-    } catch(NumberFormatException e) { 
-      return false; 
-    } catch(NullPointerException e) {
-      return false;
-    }
-    // only got here if we didn't return false
-    return true;
-  }
-  public boolean playerVal(int v, int j){
-  for(Player p: parentGame.getPlayers()){
-    if(!p.equals(this)){
-      if( p.x == v && p.y == j){
-        return true; 
-      }
-    }
-  }
-  return false;
-  }
-  public void move(int vx,int vy){
-   try{
-        String val = currentMap[y+vy][x+vx];
-        if((" P 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18").indexOf(val) != -1 &&!playerVal(vx+x,vy+y)){//I die a little every time          
-          x += vx;
-          y += vy;
-        }
-        else if(val.equals("!")){
-          parentGame.setPlayersDone(parentGame.getPlayersDone()+1);
-          parentGame.getPlayers().remove(parentGame.getPlayers().indexOf(this));
-          if(parentGame.getPlayersDone() >= parentGame.getTotalPlayers()){
-            parentGame.setLevel(parentGame.getLevel()+1);
-            parentGame.initialize();
-          }
-        }
-        else if(val.equals("x")){
-          parentGame.initialize();
-        }
-        for(Enemy e:parentGame.getEnemies()){
-          if(x == e.x && e.y == y)
-            parentGame.initialize();
-        }
-      }
-      catch(Exception e){
-        parentGame.getPlayers().remove(parentGame.getPlayers().indexOf(this));
-      } 
-  }
+public class Player extends Point {
+	Game parentGame;
+	String[][] currentMap;
+
+	public Player(int x, int y, Game g) {
+		super(x, y);
+		parentGame = g;
+		currentMap = parentGame.getCurrentMap();
+	}
+
+	public void checkEnemyTouch() {
+		for (Enemy e : parentGame.getEnemies()) {
+			if (x == e.x && y == e.y) {
+				parentGame.initialize();
+			}
+		}
+	}
+
+	public static boolean isInteger(String s) {
+		try {
+			Integer.parseInt(s);
+		} catch (NumberFormatException e) {
+			return false;
+		} catch (NullPointerException e) {
+			return false;
+		}
+		// only got here if we didn't return false
+		return true;
+	}
+
+	public boolean playerVal(int v, int j) {
+		for (Player p : parentGame.getPlayers()) {
+			if (!p.equals(this)) {
+				if (p.x == v && p.y == j) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	public void move(int vx, int vy) {
+		try {
+			String val = currentMap[y + vy][x + vx];
+			if ((" P 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18").indexOf(val) != -1 && !playerVal(vx + x, vy + y)) {// I
+																														// die
+																														// a
+																														// little
+																														// every
+																														// time
+				x += vx;
+				y += vy;
+			} else if (val.equals("!")) {
+				parentGame.setPlayersDone(parentGame.getPlayersDone() + 1);
+				parentGame.getPlayers().remove(parentGame.getPlayers().indexOf(this));
+				if (parentGame.getPlayersDone() >= parentGame.getTotalPlayers()) {
+					parentGame.setLevel(parentGame.getLevel() + 1);
+					parentGame.initialize();
+				}
+			} else if (val.equals("x")) {
+				parentGame.initialize();
+			}
+			for (Enemy e : parentGame.getEnemies()) {
+				if (x == e.x && e.y == y)
+					parentGame.initialize();
+			}
+		} catch (Exception e) {
+			parentGame.getPlayers().remove(parentGame.getPlayers().indexOf(this));
+		}
+	}
 }
